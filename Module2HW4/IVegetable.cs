8b5a544a90f760ccf6d4cc5b4d6cdcbe84258898
Roman Nikitin <roman.nikitin@internera.systems>
2024-04-24 20:44:05 +0300

    solution

---
+++ Module2HW4/IVegetable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module2HW4
+{
+    public interface IVegetable
+    {
+        string Name { get; }
+        int Calories { get; }
+    }
+}
---
+++ Module2HW4/RootVegetable.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module2HW4
+{
+    public class RootVegetable : EdibleVegetable
+    {
+        public RootVegetable(string name, int calories) : base(name, calories)
+        {
+        }
+
+        public override int CompareTo(EdibleVegetable other)
+        {
+            return this.Calories.CompareTo(other.Calories);
+        }
+    }
+}
